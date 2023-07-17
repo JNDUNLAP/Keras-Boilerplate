27e274388569e2790dc02b6da9a40e5d0c4400ee
Jack Dunlap <jdunlap@givens.com>
2023-07-17 12:50:39 -0400

    public ready

--- Dockerfile
+++ Dockerfile
@@ -2,18 +2,17 @@ FROM python:3.10.0
 
 WORKDIR /app
 
-COPY src/requirements.txt ./requirements.txt
+COPY requirements.txt ./requirements.txt
 
 RUN pip install --no-cache-dir -r requirements.txt && pip install uwsgi
 
-RUN useradd -m myuser
+RUN useradd -m givens
 
-USER myuser
+USER givens
 
-COPY src/ ./
+COPY . ./
 
 EXPOSE 8080
 
-HEALTHCHECK CMD curl --fail http://localhost:8080/health || exit 1
 
 CMD ["uwsgi", "--ini", "app.ini"]
--- app.py
+++ app.py
@@ -1,14 +1,25 @@
 from flask import request, Flask, jsonify
 import numpy as np
-
+import tensorflow as tf
 import os
 from datetime import datetime
-from src.model import loaded_model
 from dotenv import load_dotenv
 load_dotenv()
+MODEL_PATH = 'Models/model.keras'
+
+# Load model outside 
+def load_model(model_path):
+    model = tf.keras.models.load_model(model_path, compile=False)
+    model.compile(loss=tf.keras.losses.BinaryCrossentropy(from_logits=True), optimizer=tf.keras.optimizers.Adam(1e-4),  metrics=['accuracy'])
+    return model
+
+loaded_model = load_model(MODEL_PATH)
+
+
 
 app = Flask(__name__)
 PORT = int(os.getenv('PORT'))
+
 
 
 def validate_input(text):
